71093364bd24eda3e39b403b45847b851a9be0ae
ThiagoJunkes <thiago.junkes3@gmail.com>
2023-06-28 21:45:37 -0300

    Telas feitas

--- app/src/main/java/org/udesc/program/EditActivity.java
+++ app/src/main/java/org/udesc/program/EditActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ViewExpenseActivity extends AppCompatActivity {
+public class EditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
